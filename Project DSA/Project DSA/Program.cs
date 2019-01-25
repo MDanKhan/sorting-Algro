c722cf5b4edab61d5833815151e05a586f3d1883
mdankha <47027222+mdankha@users.noreply.github.com>
2019-01-25 06:05:23 -0800

    Add files via upload

---
+++ Project DSA/Project DSA/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_DSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Lenght of Array:");
+            int num = Convert.ToInt16(Console.ReadLine());
+            int[] arr = new int[num];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = 0;
+            Console.WriteLine("Enter Elements in Array");
+            for (int i = 0; i < num; i++)
+            {
+                if (i > num)
+                    Console.WriteLine("Array Out Of Bond");
+                else
+                    arr[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            //Object of pigeonhole sort class. 
+            PigeonHole_Sort ph = new PigeonHole_Sort();
+            arr = ph.pigeonhole_sort(arr, num);
+            ph.Print(arr);
+        }
+    }
+    class PigeonHole_Sort
+    {
+        public int[] pigeonhole_sort(int[] arr, int n)
+        {
+            int min = arr[0];
+            int max = arr[0];
+            int range, i, j, index;
+
+            //To Find Min and Max in Array.
+            for (int a = 0; a < n; a++)
+            {
+                if (arr[a] > max)
+                    max = arr[a];
+                if (arr[a] < min)
+                    min = arr[a];
+            }
+
+            //Initializing Range.
+            range = max - min + 1;
+
+            //Container Array.
+            int[] phole = new int[range];
+
+            //initializing Phole array elements to zero.
+            for (i = 0; i < n; i++)
+                phole[i] = 0;
+
+            //PigeonHole Sort.
+            for (i = 0; i < n; i++)
+                phole[arr[i] - min]++;
+
+            index = 0;
+            for (j = 0; j < range; j++)
+                while (phole[j]-- > 0)
+                    arr[index++] = j + min;
+
+
+            return arr;
+        }
+        //Function to print sorted array.
+        public void Print(int[] arr)
+        {
+            Console.WriteLine("Sorted Array:");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(+arr[i] + " ");
+        }
+    }
+}
---
+++ Project DSA/README.txt.txt
@@ -0,0 +1,23 @@
+#PigeonHole Sort
+
+*Implementaion of Pigeonhole sort in C# language,console based program using VisualStudio(IDE). 
+*For academic purpose only.
+*pigeonhole sorting algorithm is used to sort array elements.
+ In O(n+N)time complexity.
+*Given JPG file contains description of project.
+*All files related to project are included.
+
+*Platform:
+-Visual Studio (IDE)
+
+*How to run?
+-Enter number of element to be entered.
+-Enter unsorted elements in array and get desired ouput of sorted array.
+
+*Works for?
+-All negative and positive integer values.
+
+Group Members:
+Muhammad Ali Hassan	17B-024-SE
+Muhammad Daniyal Khan	17B-059-SE
+Mirza Khizer Baig	17B-039-SE 
